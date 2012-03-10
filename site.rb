b11070057b27d58564d5e535055a99a586dae018
Wei Hsu <wei@airtime.com>
2012-03-10 13:46:17 -0800

    init

---
+++ site.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'sinatra'
+
+get '/' do
+  erb :home
+end
+
+get '/about' do
+  @name = 'Wei'
+
+  @seconds_old = Time.now - Time.local(1984, 4, 20)
+  @age = @seconds_old / 365 / 24 / 60 / 60
+  erb :about
+end
+
+get '/work' do
+  erb :work
+end
+
+not_found do
+  '404!'
+end
